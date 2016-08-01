59680beccf18134455b64465fc77c149352829f0
Petros <snoopy@theParticle.local>
2016-08-01 18:49:07 +0200

    Fixed the path in case of installing it in the engine

--- lib/generators/devise/views/bootstrap_templates/bootstrap_templates_generator.rb
+++ lib/generators/devise/views/bootstrap_templates/bootstrap_templates_generator.rb
@@ -3,8 +3,21 @@ module Devise
     class BootstrapTemplatesGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../../../app/views', __FILE__)
       def copy_views
-         directory("devise", Rails.root.join("app", "views", "devise"))
+        binding.pry
+        directory("devise", the_root_path)
       end
+
+      private
+
+      def the_root_path
+        if ENGINE_ROOT
+          engine_name = ENGINE_ROOT.rpartition('/').last.underscore
+          Pathname.new(ENGINE_ROOT).join("app", "views", engine_name, "devise")
+        else
+          Rails.root.join("app", "views", "devise")
+        end
+      end
+
     end
   end
 end
